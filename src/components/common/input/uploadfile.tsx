0274ff63bd342c14c9e9ee8497f13012b78e8d22
surendra.patidar <suredra.patidar@photon.com>
2023-06-19 19:10:51 +0530

    file reading without any extra component

--- src/components/common/input/uploadfile.tsx
+++ src/components/common/input/uploadfile.tsx
@@ -31,7 +31,6 @@ function UploadFile() {
             type="radio"
             value="comma"
             defaultChecked
-            onChange={(e) => handleChange(e)}
             name="inline-radio-group"
             className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
           />
@@ -48,7 +47,6 @@ function UploadFile() {
             type="radio"
             value="pipeline"
             name="inline-radio-group"
-            onChange={(e) => handleChange(e)}
             className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
           />
           <label
